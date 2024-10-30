a647b2d78ef9b031743e7eb81f6b81ff6eb6e279
Luiz Desuó N <luizdesuo@gmail.com>
2024-10-30 16:19:35 -0300

    feat: remove bernstein_density and add Bernstein numpyro model

--- src/banquo/numpyro.py
+++ src/banquo/numpyro.py
@@ -8,11 +8,10 @@
 
 from dataclasses import dataclass, field
 
-import numpyro
 from array_api_compat import array_namespace
-from numpyro.distributions import Beta, Dirichlet
+from numpyro.distributions import Beta, Distribution, constraints
 
-from banquo import array, bernstein_lpdf, shape_handle_wT, shape_handle_x
+from banquo import array, bernstein_cdf, bernstein_lpdf, shape_handle_wT, shape_handle_x
 
 
 ###############################################################################
@@ -68,57 +67,116 @@ class NumpyroBeta:
 ###############################################################################
 
 
-def bernstein_density(x: array, zeta: array) -> None:
-    """Compute the Bernstein polynomial-based lpdf in a Numpyro model.
+# TODO: Combine this with Dirichlet distribution
+class Bernstein(Distribution):  # type: ignore
+    """Bernstein polynomial-based model.
 
-    This function samples weights `w` from a Dirichlet distribution with
-    concentration parameter `zeta` and computes the log-probability of
-    observing the data `x` using a Bernstein polynomial likelihood
-    function. It adds the resulting lpdf as a factor to the joint
-    log-probability in a NumPyro model. This function does not return any value.
-    Instead, it affects the state of the NumPyro probabilistic model by
-    adding `w` to the model trace and modifying the joint log-probability.
+    The `Bernstein` class implements a probability distribution
+    for nonparametric modeling densities with Bernstein polynomials.
+    It uses a set of `weights` as coefficients for the basis functions
+    and supports computing the log-probability density and cumulative
+    distribution function (CDF).
 
     Parameters
     ----------
-    x : array
-        The observed data or values to evaluate the lpdf. The array should
-        have shape `(n, d)`, where `n` is the number of samples,
-        and `d` is the number of dimensions. If `x` is one-dimensional
-        with shape `(n,)`, it will be reshaped to `(n, 1)`. Each element
-        represents a sample to be evaluated under the Bernstein polynomial
-        model.
-    zeta : array
-        The concentration parameter of the Dirichlet distribution.
-        It controls the distribution of the simplex weights `w`.
-        The array should have shape `(d, k)`, where `k` is the number of
-        basis functions (order of the Bernstein polynomial) and `d` the
-        number of dimensions. If the shape is `k`, the system will be
-        considered as a one-dimensional array.
+    weights : array
+        Array of weights (simplex) with shape `(d, k)`, where `d` is the number
+        of dimensions, and `k` is the number of basis functions (Bernstein
+        polynomial order). If the shape is `k`, the system will be considered
+        as a one-dimensional array. The weights are, for each dimension `d`,
+        a `k`-dimensional unit simplex. The weights are applied as coefficients
+        for the Bernstein polynomial basis in each dimension.
 
-    Side Effects
-    ------------
-    - Samples weights `w` from a Dirichlet distribution using
-      :func:`numpyro.sample`.
-    - Adds a log-probability factor to the joint model log-probability
-      using :func:`numpyro.factor`. The factor is computed using the
-      :func:`bernstein_lpdf` function.
+    validate_args : bool | None, optional
+        If True, validates the input parameters. By default, None (no
+        validation is applied).
+
+    Raises
+    ------
+    ValueError
+        If `weights` is not at least one-dimensional.
 
     Notes
     -----
-    - The Dirichlet distribution samples weights `w` sum to 1 across
-      across each dimension. They are used as coefficients for the
-      Bernstein polynomial model.
-    - The :func:`bernstein_lpdf` function is called to compute the
-      log-likelihood of the observed data `x` given the sampled weights `w`.
-      This likelihood function relies on a Beta distribution
-      parameterization.
+    - :func:`Bernstein.log_prob` and :func:`Bernstein.cdf` use
+      :func:`banquo.bernstein_lpdf` and :func:`banquo.bernstein_cdf` respectively, with
+      Beta distribution based Bernstein basis functions.
+    - The weights must sum to 1 across each dimension, fulfilling the
+      Dirichlet distribution constraint.
     """
-    # Sample 'w' from the Dirichlet distribution
-    w = numpyro.sample("w", Dirichlet(zeta))
 
-    # Add a factor that affects the joint log-probability
-    numpyro.factor(
-        "bernstein_lpdf",
-        bernstein_lpdf(NumpyroBeta, shape_handle_x(x), shape_handle_wT(w)),
-    )
+    arg_constraints = {
+        "weights": constraints.simplex,
+    }
+    reparametrized_params = ["weights"]
+    support = constraints.unit_interval
+
+    def __init__(self, weights: array, *, validate_args: bool | None = None) -> None:
+        """Init Bernstein model."""
+        if weights.ndim < 1:
+            raise ValueError("`weights` parameter must be at least one-dimensional.")
+
+        self.weights = weights
+
+        # batch_shape = d, event_shape = k
+        batch_shape, _ = weights.shape[:-1], weights.shape[-1:]
+        super().__init__(
+            batch_shape=batch_shape,
+            validate_args=validate_args,
+        )
+
+    def log_prob(self, value: array) -> array:
+        """Compute the lpdf of `value` using the Bernstein polynomial model.
+
+        Parameters
+        ----------
+        value : array
+            The observed data or values to evaluate the lpdf. The array should
+            have shape `(n, d)`, where `n` is the number of samples,
+            and `d` is the number of dimensions. If `value` is one-dimensional
+            with shape `(n,)`, it will be reshaped to `(n, 1)`. Each element
+            represents a sample to be evaluated under the Bernstein polynomial
+            model.
+
+        Returns
+        -------
+        array
+            The log-probability density function of `value`
+            under the Bernstein model.
+
+        Notes
+        -----
+        The :func:`banquo.bernstein_lpdf` function is used to calculate the lpdf,
+        based on Beta distribution for Bernstein basis functions.
+        """
+        return bernstein_lpdf(
+            NumpyroBeta, shape_handle_x(value), shape_handle_wT(self.weights)
+        )
+
+    def cdf(self, value: array) -> array:
+        """Compute the cdf of `value` using the Bernstein polynomial model.
+
+        Parameters
+        ----------
+        value : array
+            The observed data or values to evaluate the lpdf. The array should
+            have shape `(n, d)`, where `n` is the number of samples,
+            and `d` is the number of dimensions. If `value` is one-dimensional
+            with shape `(n,)`, it will be reshaped to `(n, 1)`. Each element
+            represents a sample to be evaluated under the Bernstein polynomial
+            model.
+
+        Returns
+        -------
+        array
+            The cumulative distribution function of `value`
+            under the Bernstein model.
+
+        Notes
+        -----
+        The :func:`banquo.bernstein_cdf` function is used to calculate the cdf,
+        based on Beta distribution for Bernstein basis functions.
+        """
+        return bernstein_cdf(
+            NumpyroBeta, shape_handle_x(value), shape_handle_wT(self.weights)
+        )
