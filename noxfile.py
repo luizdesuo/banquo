638f8da4477bbdf838d47913f5ab9cc49c7702f3
Luiz Desuó N <luizdesuo@gmail.com>
2024-11-05 18:28:00 -0300

    build: safety check was deprecated, updated to scan

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,8 @@
 """Nox sessions."""
+
 from pathlib import Path
+from typing import no_type_check
+
 import nox
 
 
@@ -14,30 +17,36 @@ nox.options.sessions = (
 )
 
 
+@no_type_check
 @nox.session(python=_python_versions)
 def safety(session: nox.Session) -> None:
     """Scan dependencies for insecure packages."""
     session.install(".[test]")
-    session.run("safety", "check", "--full-report")
+    session.run("safety", "scan", "--full-report")
 
+
+@no_type_check
 @nox.session(python=_python_versions)
 def typeguard(session: nox.Session) -> None:
     """Runtime type checking using Typeguard."""
     session.install(".[test]")
     session.run("pytest", f"--typeguard-_packages={_package}", *session.posargs)
 
+
+@no_type_check
 @nox.session(python=_python_versions)
 def tests(session: nox.Session) -> None:
     """Run the test and xdoctest suites."""
     session.install(".[test]")
-    session.run("pytest",
-                "--cov=banquo",
-                "--cov-report=xml",
-                "--xdoctest",
-                *session.posargs)
+    session.run(
+        "pytest", "--cov=banquo", "--cov-report=xml", "--xdoctest", *session.posargs
+    )
 
+
+@no_type_check
 @nox.session
 def docs(session: nox.Session) -> None:
+    """Build documentation."""
     with open(Path("docs/requirements.txt")) as f:
         requirements = f.read().splitlines()
     session.install(".[docs]")
