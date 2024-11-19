e64fa93edfc320fb565041df3e87395ac2b2f296
Luiz Desuó N <luizdesuo@gmail.com>
2024-11-19 17:54:46 -0300

    fix: populate namespace

--- src/banquo/__init__.py
+++ src/banquo/__init__.py
@@ -2,7 +2,11 @@
 
 # read version from installed package
 from importlib.metadata import version
+
+
 __version__ = version("banquo")
 
 # populate package namespace
-from banquo.banquo import *
+from banquo.banquo import *  # noqa: F401, F403
+from banquo.kernels import *  # noqa: F401, F403
+from banquo.numpyro import *  # noqa: F401, F403
